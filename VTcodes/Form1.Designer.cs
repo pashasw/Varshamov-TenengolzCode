5145bbc94aab470ac4c2e85be0b2f9f6e875f7ae
pashasw <pashasw@gmail.com>
2018-01-12 18:23:35 +0300

    first commit

---
+++ VTcodes/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace VTcodes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenCode = new System.Windows.Forms.Button();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Fix = new System.Windows.Forms.Button();
+            this.textBoxFixed = new System.Windows.Forms.TextBox();
+            this.textBoxTypeError = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GenCode
+            // 
+            this.GenCode.Location = new System.Drawing.Point(12, 51);
+            this.GenCode.Name = "GenCode";
+            this.GenCode.Size = new System.Drawing.Size(130, 23);
+            this.GenCode.TabIndex = 0;
+            this.GenCode.Text = "Генерация слова";
+            this.GenCode.UseVisualStyleBackColor = true;
+            this.GenCode.Click += new System.EventHandler(this.GenCode_Click);
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(12, 26);
+            this.textBoxN.Multiline = true;
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(29, 19);
+            this.textBoxN.TabIndex = 1;
+            this.textBoxN.Text = "7";
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(12, 120);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(130, 19);
+            this.textBoxCode.TabIndex = 2;
+            this.textBoxCode.Text = "00110100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Code";
+            // 
+            // Fix
+            // 
+            this.Fix.Location = new System.Drawing.Point(12, 145);
+            this.Fix.Name = "Fix";
+            this.Fix.Size = new System.Drawing.Size(130, 22);
+            this.Fix.TabIndex = 5;
+            this.Fix.Text = "Исправить";
+            this.Fix.UseVisualStyleBackColor = true;
+            this.Fix.Click += new System.EventHandler(this.Fix_Click);
+            // 
+            // textBoxFixed
+            // 
+            this.textBoxFixed.Location = new System.Drawing.Point(192, 23);
+            this.textBoxFixed.Multiline = true;
+            this.textBoxFixed.Name = "textBoxFixed";
+            this.textBoxFixed.Size = new System.Drawing.Size(130, 144);
+            this.textBoxFixed.TabIndex = 6;
+            // 
+            // textBoxTypeError
+            // 
+            this.textBoxTypeError.Location = new System.Drawing.Point(148, 120);
+            this.textBoxTypeError.Multiline = true;
+            this.textBoxTypeError.Name = "textBoxTypeError";
+            this.textBoxTypeError.Size = new System.Drawing.Size(29, 19);
+            this.textBoxTypeError.TabIndex = 7;
+            this.textBoxTypeError.Text = "0->1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 179);
+            this.Controls.Add(this.textBoxTypeError);
+            this.Controls.Add(this.textBoxFixed);
+            this.Controls.Add(this.Fix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.GenCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenCode;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Fix;
+        private System.Windows.Forms.TextBox textBoxFixed;
+        private System.Windows.Forms.TextBox textBoxTypeError;
+    }
+}
+
---
+++ VTcodes/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VTcodes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string[] CombinationCodes(int n, int l)
+        {
+            String[] Codes = new String[(int)Math.Pow(2, n)];
+            for (int i = 0; i < Codes.Length; i++)
+                Codes[i] = "";
+            int step = (int)Math.Pow(2, n - 1);
+            for (int i = 0; i < n; i++)
+            {
+                char current = '0';
+                for (int j = 1; j <= Codes.Length; j++)
+                {
+                    Codes[j - 1] += current;
+                    if (j % step == 0)
+                    {
+                        if (current == '0') current = '1';
+                        else current = '0';
+                    }
+                }
+                step = step / 2;
+            }
+            String[] VTCodes = new String[Codes.Length];
+            int k = 0;
+            for (int i = 0; i < Codes.Length; i++)
+            {
+                int s = 0;
+                for (int j = 0; j < n; j++)
+                    s += Codes[i][j] * (j + 1);
+                if (s % l == 0)
+                    VTCodes[k++] = Codes[i];
+            }
+            String[] output = new String[k];
+            for (int i = 0; i < k; i++)
+                output[i] = VTCodes[i];
+            return output;
+        }
+        private void GenCode_Click(object sender, EventArgs e)
+        {
+            string[] codes = CombinationCodes(Convert.ToInt32(textBoxN.Text), Convert.ToInt32(textBoxN.Text) + 1);
+            Random rand = new Random();
+            textBoxCode.Text = codes[rand.Next(0,codes.Count() -1)];
+        }
+        public static string XOR(string R, string key)
+        {
+            string newkey = "";
+            if (R.Length == key.Length)
+            { newkey = key; }
+            string answer = "";
+            for (int i = 0; i < R.Length; i++)
+            {
+                if ((R[i] == '1' && newkey[i] == '1') || (R[i] == '0' && newkey[i] == '0'))
+                    answer += '0';
+                if ((R[i] == '0' && newkey[i] == '1') || (R[i] == '1' && newkey[i] == '0'))
+                    answer += '1';
+            }
+            return answer;
+        }
+        public static string FixMyCode(string code, string[] codes, int n, int l, int typeerror)
+        {
+            string answer = "";
+            if (typeerror == 0)//0->1
+            {
+                int s =0;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    s += (Convert.ToInt32(code[i]) - '0') * (i + 1);
+                }
+                int k = s % l;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    if (i == k - 1)
+                    { answer += "0";}
+                    else { answer += code[i]; }
+                }
+            }
+            if (typeerror == 1)//1->0
+            {
+                int s = 0;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    s += (Convert.ToInt32(code[i]) - '0') * (i + 1);
+                }
+                int k = (s) % l;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    if (i == (-1 * k + n))
+                    { answer += "1"; }
+                    else { answer += code[i]; }
+                }
+            }
+            if (typeerror == 2)
+            {
+                int w = 0;
+                int s = 0;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    s += (Convert.ToInt32(code[i]) - '0') * (i + 1);
+                    w += Convert.ToInt32(code[i]) - '0';
+                }
+                int k = s % l;
+
+                if (k == 0)
+                {
+                    for (int i = 0; i < code.Length - 1; i++)
+                        answer += code[i];
+                }
+                if (k == w)
+                {
+                    for (int i = 1; i < code.Length; i++)
+                        answer += code[i];
+                }
+
+                if (k < w && k > 0)
+                {
+                    string temp = "";
+                    temp += code[0];
+                    answer += code[0];
+                    int sumzero = 0;
+                    for (int i = 1; i < code.Length - 1; i++)
+                    {
+                        int j = i + 1;
+                        if (code[i] == '0')
+                        {
+                            while (j < code.Length && sumzero != k)
+                            {
+                                sumzero += (Convert.ToInt32(code[j]) - '0') % 2;
+                                j++;
+                            }
+                            temp += code[i];
+
+                            i++;
+                            while (i < code.Length)
+                            { answer += code[i]; i++; }
+                            //break;
+                        }
+                        else { answer += code[i]; temp += code[i]; continue; }
+                        int sum =0;
+                        for (int p=0; p < answer.Length; p++)
+                        {
+                             sum += (Convert.ToInt32(answer[p]) - '0') * (p + 1);
+                        }
+                        if (sum % (n + 1) == 0)
+                        { return answer; }
+                        else { sumzero = 0; sum = 0; answer = ""; answer = temp + code[temp.Length]; temp= answer; ; i = temp.Length-1; }
+                    }
+
+                    }
+                    
+            
+                if (k > w)
+                {
+                    string temp = "";
+                    temp += code[0];
+                    answer += code[0];
+                    int sumzero = 0;
+                    for (int i = 1; i < code.Length - 1; i++)
+                    {
+                        int j = i + 1;
+                        if (code[i] == '1')
+                        {
+                            while (j < code.Length && sumzero != n + 1 - k)
+                            {
+                                sumzero += (Convert.ToInt32(code[j]) - '0' + 1) % 2;
+                                j++;
+                            }
+                            temp += code[i];
+
+                            i++;
+                            while (i < code.Length)
+                            { answer += code[i]; i++; }
+                            //break;
+                        }
+                        else { answer += code[i]; temp += code[i]; continue; }
+                        int sum =0;
+                        for (int p=0; p < answer.Length; p++)
+                        {
+                             sum += (Convert.ToInt32(answer[p]) - '0') * (p + 1);
+                        }
+                        if (sum % (n + 1) == 0)
+                        { return answer; }
+                        else { sumzero = 0; sum = 0; answer = ""; answer = temp + code[temp.Length]; temp= answer; ; i = temp.Length-1; }
+                    }
+
+                }
+            }
+            return answer;
+        }
+        public static string FixMyCode2(string code, string[] codes, int n, int l, int typeerror)
+        {
+            string answer = "";
+            if (typeerror == 0)//0->1
+            {
+                int s = 0;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    s += (Convert.ToInt32(code[i]) - '0') * (i + 1);
+                }
+                int k = s % l;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    if (i == k - 1)
+                    { answer += "0"; }
+                    else { answer += code[i]; }
+                }
+            }
+            if (typeerror == 1)//1->0
+            {
+                int s = 0;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    s += (Convert.ToInt32(code[i]) - '0') * (i + 1);
+                }
+                int k = (s) % l;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    if (i == (-1 * k + n))
+                    { answer += "1"; }
+                    else { answer += code[i]; }
+                }
+            }
+            if (typeerror == 2)
+            {
+                int w = 0;
+                int s = 0;
+                for (int i = 0; i < code.Length; i++)
+                {
+                    s += (Convert.ToInt32(code[i]) - '0') * (i + 1);
+                    w += Convert.ToInt32(code[i]) - '0';
+                }
+                int k = s % l;
+
+                if (k == 0)
+                {
+                    for (int i = 0; i < code.Length - 1; i++)
+                        answer += code[i];
+                }
+                if (k == w)
+                {
+                    for (int i = 1; i < code.Length; i++)
+                        answer += code[i];
+                }
+
+                if (k < w && k > 0)
+                {
+                    string temp = "";
+                    temp += code[0];
+                    answer += code[0];
+                    int sumzero = 0;
+                    for (int i = 1; i < code.Length - 1; i++)
+                    {
+                        int j = i + 1;
+                        if (code[i] == '0')
+                        {
+                            while (j < code.Length)
+                            {
+                                sumzero += (Convert.ToInt32(code[j]) - '0') % 2;
+                                j++;
+                            }
+                            if (sumzero == k)
+                            { i++; while (i < code.Length) { answer += code[i]; i++; } }
+                            else { answer += code[i]; sumzero = 0; }
+                        }
+                        else { answer += code[i]; }
+
+                    }
+
+                }
+
+
+                if (k > w)
+                {
+                    string temp = "";
+                    temp += code[0];
+                    answer += code[0];
+                    int sumzero = 0;
+                    for (int i = 1; i < code.Length - 1; i++)
+                    {
+                        int j = i + 1;
+                        if (code[i] == '1')
+                        {
+                            while (j < code.Length)
+                            {
+                                sumzero += (Convert.ToInt32(code[j]) - '0'+1) % 2;
+                                j++;
+                            }
+                            if (sumzero == n+1-k)
+                            { i++; while (i < code.Length) { answer += code[i]; i++; } }
+                            else { answer += code[i]; sumzero =0; }
+                        }
+                        else { answer += code[i]; }
+
+                    }
+
+                }
+                    
+
+                
+            }
+            return answer;
+        }
+        private void Fix_Click(object sender, EventArgs e)
+        {
+            string[] codes = CombinationCodes(Convert.ToInt32(textBoxN.Text), Convert.ToInt32(textBoxN.Text) + 1);
+            string mycode = textBoxCode.Text;
+            int n = Convert.ToInt32(textBoxN.Text);
+            int typeerror = -1 ;
+            if (mycode.Length == n && textBoxTypeError.Text == "0->1") typeerror = 0;
+            if (mycode.Length == n && textBoxTypeError.Text == "1->0") typeerror = 1;
+            if (mycode.Length == n + 1) typeerror = 2;
+            textBoxFixed.Text = FixMyCode2(mycode, codes, n, n + 1, typeerror);
+        }
+    }
+}
